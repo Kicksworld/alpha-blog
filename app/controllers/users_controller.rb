9de4ad36e59b30104fe63445fac68c3b925b30c6
Vivek <vivekwagh63@gmail.com>
2020-08-25 01:45:35 +0530

    Add sign up via backend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,11 +7,17 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-        flash[:notice] = "User is successfully created"
-        redirect_to @user
+        flash[:notice] = "Welcome #{@user.username}, you have successfully signed up"
+        redirect_to articles_path
         else
             render 'new'
         end
+    end
+
+    private
+
+    def user_params
+    params.require(:user).permit(:username, :email, :password)
     end
 
     def update
