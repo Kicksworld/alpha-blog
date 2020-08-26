02efc711b4242110d27496fd57cbecdf9d8dee1f
Vivek <vivekwagh63@gmail.com>
2020-08-27 02:48:57 +0530

    Add Pagination and user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,33 +1,37 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update, :destroy]
+
 
     def new
         @user = User.new
     end
 
     def show
-        @user = User.find(params[:id])
-        @articles = @user.articles
+        @pagy, @articles = pagy(@user.articles, items: 5)
+    end
+
+    def index
+        @pagy, @user = pagy(User.all, items: 5)
     end
 
     def edit   
-        @user = User.find(params[:id])
+       
     end
 
     def create
         @user = User.new(user_params)
         if @user.save
         flash[:notice] = "Welcome #{@user.username}, you have successfully signed up"
-        redirect_to articles_path
+        redirect_to @user
         else
             render 'new'
         end
     end
 
     def update
-        @user = User.find(params[:id])
         if @user = User.update(user_params)
             flash[:notice] = "User details has been successfully updated"
-             redirect_to articles_path
+             redirect_to user_path
              else
                 render 'edit'
              end    
@@ -35,6 +39,10 @@ class UsersController < ApplicationController
 
     private
 
+    def set_user
+        @user = User.find(params[:id])
+    end
+    
     def user_params
     params.require(:user).permit(:username, :email, :password)
     end
